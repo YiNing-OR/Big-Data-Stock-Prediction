60c63a14153727dac06d2e8ef9e971b151947a23
Benjamin Ho <bhgm93@gmail.com>
2024-10-26 11:28:52 +0800

    update agent and environment

--- agent.py
+++ agent.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from environment import StockTradingEnv
 from tqdm import tqdm
-import time
+import random
+
+random.seed(42)
 
 class QLearningAgent():
     def __init__(
@@ -14,7 +16,7 @@ class QLearningAgent():
                     decay=0.001, 
                     min_exploration_prob=0.01,
                     gamma=0.9,
-                    lr=0.001
+                    lr=0.1
     ):
         self.n_actions = n_actions
         self.n_states = n_states
@@ -22,16 +24,20 @@ class QLearningAgent():
         self.total_rewards = list()
         self.exploration_prob = exploration_prob
         self.min_exploration_prob = min_exploration_prob
-        self.env = StockTradingEnv(df, n_actions, n_states, starting_value=10000)
+        self.env = StockTradingEnv(df, n_actions, n_states, starting_value=5000)
         self.lr = lr
         self.gamma = gamma
         self.decay = decay
 
     def episode(self, current_state, episode_num):
-        if np.random.uniform(0, 1) < self.exploration_prob:
+        if episode_num == 0:
             action = self.env.sample_action(episode_num)
         else:
-            action = np.argmax(self.q_table[current_state,:])
+            if np.random.uniform(0, 1) < self.exploration_prob:
+                action = self.env.sample_action(episode_num)
+            else:
+                action = np.argmax(self.q_table[current_state,:])
+                action = self.env.verify_action(action, episode_num)
         
         next_state, reward = self.env.step(action, episode_num)
 
@@ -40,18 +46,49 @@ class QLearningAgent():
         return next_state, reward
     
     def simulate(self):
-        current_state = 5
-        for e in tqdm(range(self.env.num_episodes)):
+        current_state = 6
+        self.env.reset()
+        for e in range(self.env.num_episodes):
+            # print(f"==== Episode {e} ====")
             next_state, reward = self.episode(current_state, e)
             self.exploration_prob = max(self.min_exploration_prob, np.exp(-self.decay * e))
-            self.total_rewards.append(reward)
             current_state = next_state
-            # time.sleep(0.1)
+
+    def train_agent(self):
+        num_epochs = 1000
+        self.total_rewards = []
+        for ep in tqdm(range(num_epochs)):
+            self.simulate()
+            total = sum(self.env.reward_historical)
+            self.total_rewards.append(total)
+        plt.plot(self.total_rewards)
+        plt.show()
+
     
     def render(self):
         print(self.q_table)
-        plt.plot(self.env.portfolio_value_historical)
-        plt.show()
+        # plt.plot(self.env.reward_pct_historical)
+        # plt.show()
+
+        # plt.plot(self.env.reward_historical)
+        # plt.show()
+
+        fig, ax1 = plt.subplots()
+        # Plot the first line chart on the primary y-axis
+        ax1.plot(self.env.portfolio_value_historical, 'b-', label='Portfolio Value')
+        ax1.set_xlabel('Days')
+        ax1.set_ylabel('Portfolio Value', color='b')
+        ax1.tick_params(axis='y', labelcolor='b')
+
+        # Create a second y-axis
+        ax2 = ax1.twinx()
+        ax2.plot(self.env.historical_close, 'r-', label='Daily Closing Price')
+        ax2.set_ylabel('Daily Close', color='r')
+        ax2.tick_params(axis='y', labelcolor='r')
+
+        # Add a title and show the plot
+        plt.title('Portfolio Value Performance vs Daily Closing')
+        # plt.show()
 
 
 if __name__ == "__main__":
@@ -63,12 +100,13 @@ if __name__ == "__main__":
     import pandas as pd
 
     df = pd.read_csv("/Users/benho/Documents/mcomp/CS5344_Project/processed_data/sti_processed.csv")
+    df = df[df['Date'] > '2024-01-01']
     # df['Pct_Change'] = df['Pct_Change'].apply(lambda x: float(x[:-1]))
     # df.to_csv("/Users/benho/Documents/mcomp/CS5344_Project/processed_data/sti_processed.csv")
     agent = QLearningAgent(df=df, n_actions=n_actions, n_states=9)
-    agent.simulate()
+    # agent.simulate()
+    agent.train_agent()
     agent.render()
-
 
 
 
--- environment.py
+++ environment.py
@@ -1,11 +1,8 @@
 import numpy as np
+import random
+import math
 
-class ActionSpace():
-    def __init__(self, n_actions):
-        self.n_actions = n_actions
-
-    def sample(self):
-        return np.random.choice(a=self.n_actions)
+random.seed(42)
 
 class StockTradingEnv():
     def __init__(self, df, n_actions, n_states, starting_value):
@@ -14,41 +11,96 @@ class StockTradingEnv():
         self.stock_value = 0
         self.num_stocks = 0
         self.n_actions = n_actions
+        self.starting_value = starting_value
         self.cash_value = starting_value
         self.portfolio_value = self.cash_value + self.stock_value
+        self.reward_historical = []
+        self.reward_pct_historical = []
+        self.pct_change_historical = []
+        self.historical_close = []
         self.portfolio_value_historical = [self.portfolio_value]
         self.state_map = np.array([[0, 1, 2],[3, 4, 5], [6, 7, 8]])
+        self.action_map = {
+            0: "Buy 1",
+            1: "Buy 2",
+            2: "Buy 3",
+            3: "Sell 1",
+            4: "Sell 2",
+            5: "Sell 3",
+            6: "Hold"
+        }
+
+    def reset(self):
+        self.reward_historical = []
+        self.reward_pct_historical = []
+        self.pct_change_historical = []
+        self.historical_close = []
+        self.stock_value = 0
+        self.num_stocks = 0
+        self.n_actions = self.n_actions
+        self.cash_value = self.starting_value
+        self.portfolio_value = self.cash_value + self.stock_value
+        self.portfolio_value_historical = [self.portfolio_value]
+
+    
+    def verify_action(self, action, e):
+        current_price = self.df.iloc[e]['Close/Last']
+        # print(f"Sampled action: {action} - ", self.action_map[action])
+        # print("Num stocks: ", self.num_stocks)
+        # print("Stock Value: ", self.stock_value)
+        # print("Cash Value: ", self.cash_value)
+        # print("Portfolio Value: ", self.portfolio_value)
+
+        if action in [0, 1, 2]:
+            max_stocks_possible = math.floor(self.cash_value / current_price)
+            num_stocks_to_buy = action + 1
+            # print(f"Max stocks buyable: {max_stocks_possible}")
+            # print(f"Number of stocks to buy: {num_stocks_to_buy}")
+
+            if max_stocks_possible < num_stocks_to_buy:
+                while max_stocks_possible < num_stocks_to_buy and action != 0:
+                    num_stocks_to_buy -= 1
+                    action -= 1
+
+            # print(f"Final action decision: {action} - {self.action_map[action]}")          
+        elif action in [3, 4, 5]:
+            max_stocks_possible = self.num_stocks
+            num_stocks_to_sell = {3: 1, 4: 2, 5: 3}[action]
+            # print(f"Number of stocks sellable: {max_stocks_possible}")
+            # print(f"Number of stocks to sell: {num_stocks_to_sell}")
+            if max_stocks_possible < num_stocks_to_sell:
+                while max_stocks_possible < num_stocks_to_sell and action != 3:
+                    num_stocks_to_sell -= 1
+                    action -= 1
+            
+            # print(f"Final action decision: {action} - {self.action_map[action]}") 
+
+        return action
 
     def sample_action(self, e):
-        current_price = self.df.iloc[e]['Close/Last']
-        if current_price > self.cash_value:
-            return np.random.choice(a=[3, 6], p=[0.3, 0.7])
-        elif current_price <= self.cash_value:
-            return np.random.choice(a=self.n_actions)
+        action = np.random.choice(a=self.n_actions)
+        return self.verify_action(action, e)
         
     def step(self, action, e):
         current_day = self.df.iloc[e]
         current_price = current_day['Close/Last']
         pct_change = current_day['Pct_Change']
 
-        new_stock_value = self.stock_value
-        new_cash_value = self.cash_value
+        # new_stock_value = self.stock_value
+        # new_cash_value = self.cash_value
 
         if action == 0: # Buy 1
-            if current_price <= self.cash_value:
-                new_cash_value = self.cash_value - (current_price * 1)
-                self.num_stocks += 1
-                new_stock_value = self.num_stocks * current_price
+            new_cash_value = self.cash_value - (current_price * 1)
+            self.num_stocks += 1
+            new_stock_value = self.num_stocks * current_price
         if action == 1: # Buy 2
-            if current_price <= self.cash_value:
-                new_cash_value = self.cash_value - (current_price * 2)
-                self.num_stocks += 2
-                new_stock_value = self.num_stocks * current_price
+            new_cash_value = self.cash_value - (current_price * 2)
+            self.num_stocks += 2
+            new_stock_value = self.num_stocks * current_price
         if action == 2: # Buy 5
-            if current_price <= self.cash_value:
-                new_cash_value = self.cash_value - (current_price * 3)
-                self.num_stocks += 3
-                new_stock_value = self.num_stocks * current_price
+            new_cash_value = self.cash_value - (current_price * 3)
+            self.num_stocks += 3
+            new_stock_value = self.num_stocks * current_price
         if action == 3: # Sell 1
             new_cash_value = self.cash_value + (current_price * 1)
             self.num_stocks -= 1
@@ -66,24 +118,38 @@ class StockTradingEnv():
             new_stock_value = self.num_stocks * current_price
         
         reward = new_stock_value + new_cash_value - self.portfolio_value
-        reward_pct = (reward - self.portfolio_value) / self.portfolio_value
-        self.portfolio_value = new_stock_value + new_cash_value
+        # print("Current portfolio value: ", self.portfolio_value)
+        # print("New portfolio value: ", new_cash_value + new_stock_value)
+        # print("Reward: ", reward)
+        reward_pct = reward / self.portfolio_value
+        # print("Reward Percentage: ", reward_pct)
+
+        self.cash_value = new_cash_value
+        self.stock_value = new_stock_value
+
+        self.portfolio_value = self.cash_value + self.stock_value
         self.portfolio_value_historical.append(self.portfolio_value)
 
-        if reward_pct >= 0.05:
+        if reward_pct >= 0.001:
             x = 0
-        elif reward_pct < 0.05 and reward_pct > -0.05:
+        elif reward_pct < 0.001 and reward_pct > -0.001:
             x = 1
-        elif reward_pct <= -0.05:
+        elif reward_pct <= -0.001:
             x = 2
 
+        self.reward_historical.append(reward)
+        self.reward_pct_historical.append(reward_pct)
+
         pct_change = pct_change / 100
-        if pct_change >= 0.05:
+        if pct_change >= 0.002:
             y = 0
-        elif pct_change < 0.05 and pct_change > -0.05:
+        elif pct_change < 0.002 and pct_change > -0.002:
             y = 1
-        elif pct_change <= -0.05:
+        elif pct_change <= -0.002:
             y = 2
+
+        self.pct_change_historical.append(pct_change)
+        self.historical_close.append(current_price)
         
         next_state = self.state_map[x, y]
 
